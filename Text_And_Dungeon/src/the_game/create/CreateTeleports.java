cb24f233bc14a9fb1324db30791845e67727b368
Giro4c <camille.girodengo@etu.univ-amu.fr>
2023-03-24 16:45:03 +0100

    upt: CreateTeleports coordinates

--- Text_And_Dungeon/src/the_game/create/CreateTeleports.java
+++ Text_And_Dungeon/src/the_game/create/CreateTeleports.java
@@ -10,14 +10,14 @@ public class CreateTeleports {
 		}
 		else if (Create.getMapID() == 2) {
 			Teleport[] teleports = new Teleport[8];
-			teleports[0] = new Teleport("Waypoint");
-			teleports[1] = new Teleport("Waypoint");
-			teleports[2] = new Teleport("Waypoint");
-			teleports[3] = new Teleport("Waypoint");
-			teleports[4] = new Teleport("Waypoint");
-			teleports[5] = new Teleport("Waypoint");
-			teleports[6] = new Teleport("Waypoint");
-			teleports[7] = new Teleport("Waypoint");
+			teleports[0] = new Teleport("Waypoint", 8, 2, 43, 14);
+			teleports[1] = new Teleport("Waypoint", 21, 6, 4, 34);
+			teleports[2] = new Teleport("Waypoint", 27, 10, 17, 39);
+			teleports[3] = new Teleport("Waypoint", 11, 13, 28, 39);
+			teleports[4] = new Teleport("Waypoint", 18, 20, 37, 31);
+			teleports[5] = new Teleport("Waypoint", 4, 21, 11, 24);
+			teleports[6] = new Teleport("Waypoint", 41, 26, 25, 31);
+			teleports[7] = new Teleport("Waypoint", 18, 43, 28, 21);
 			
 			return teleports;
 			
